c1eed84af97e0049dc1e8ef174e4078f9860a1df
Hasen Judy <hasen.judy@gmail.com>
2020-02-03 00:49:58 +0900

    list movies, find largest unconverted, launch ffmpeg on it

---
+++ main.go
@@ -0,0 +1,234 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"flag"
+	"io"
+)
+
+type VideoFile struct {
+	Dir, Name string
+	Size int // in bytes
+}
+
+type VideoFileStatus struct {
+	Name string
+	Size int
+	Src, Dst string
+
+	IsShrunk bool
+	ShrunkSize int
+
+	HadError bool // flag to avoid trying to process again
+}
+
+func ListVideos(dir string) []VideoFile {
+	f, err := os.Open(dir)
+	if err != nil {
+		fmt.Println("Error reading directory:", err)
+		os.Exit(1)
+		return nil
+	}
+	entries, err := f.Readdir(-1)
+	if err != nil {
+		fmt.Println("Error listing directory:", err)
+		os.Exit(1)
+		return nil
+	}
+	files := make([]VideoFile, 0)
+	for _, fentry := range entries {
+		name := fentry.Name()
+		ext := path.Ext(name)
+		if ext != ".mp4" { // TODO: support more extensions!
+			continue
+		}
+		size := int(fentry.Size())
+		if size < MB {
+			continue
+		}
+		vfile := VideoFile {
+			Dir: dir,
+			Name: name,
+			Size: int(fentry.Size()),
+		}
+		files = append(files, vfile)
+	}
+	return files
+}
+
+
+const KB = 1 << 10
+const MB = 1 << 20
+const GB = 1 << 30
+
+func BytesSize(size int) string {
+	if size > GB {
+		return fmt.Sprintf("%f.2 GB", float64(size) / GB)
+	}
+	if size > MB {
+		return fmt.Sprintf("%f.2 MB", float64(size) / MB)
+	}
+	if size > KB {
+		return fmt.Sprintf("%f.2 KB", float64(size) / KB)
+	}
+	return fmt.Sprintf("%f.2 B", float64(size))
+}
+
+func ShrinkMovie(movie *VideoFileStatus) error {
+	// get the video's dimensions
+	//
+	//    ffprobe -v error -select_streams v:0 -show_entries stream=width,height,duration -of default=noprint_wrappers=1:nokey=1 VID_20191207_115139.mp4
+	//    1920
+	//    1080
+	//    75.049911
+	//
+	inpath := path.Join(movie.Src, movie.Name)
+	var probeArgs = []string {
+		"-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height,nb_frames",
+		"-of", "default=noprint_wrappers=1:nokey=1",
+		inpath,
+	}
+	probeCmd := exec.Command("ffprobe", probeArgs...)
+	output, err := probeCmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("Could not get video dimensions. ffprobe command failed with: %w", err)
+	}
+
+	var width, height int
+	var duration float64
+	{
+		_, err := fmt.Sscan(string(output), &width, &height, &duration)
+		if err != nil {
+			return fmt.Errorf("Could not get video dimensions. ffprobe output parsing failed with: %w", err)
+		}
+	}
+
+	var desired_width = 1080
+	if width < height { // vertical video
+		desired_width = 720
+	}
+
+	// ffmpeg -i SRC/NAME -vf scale="DESIRED_WIDTH:-1" DST/NAME
+	outpath := path.Join(movie.Dst, movie.Name)
+	var args = []string {
+		"-y", "-i", inpath, "-vf", fmt.Sprintf(`scale=%d:-1`, desired_width), outpath,
+	}
+	cmd := exec.Command("ffmpeg", args...)
+
+	cmdout, err := cmd.StderrPipe()
+	if err != nil {
+		panic(fmt.Errorf("programmer error: incorrect usage of command piping: %w", err))
+	}
+	// reader := bufio.NewReader(cmdout)
+
+	fmt.Println(cmd.String())
+	cmd.Start()
+
+	io.Copy(os.Stdout, cmdout)
+
+	// buffer := make([]byte, 1 * KB)
+	// for {
+	// 	// just show the output for now
+	// 	n, err := cmdout.Read(buffer)
+	// 	if n > 0 {
+	// 		fmt.Printf(buffer[:n])
+	// 	}
+	// 	if err == io.EOF {
+	// 		break
+	// 	} else if err != nil {
+	// 		// unexpected error!! what could it be?!
+	// 		// TODO: return this error?
+	// 		fmt.Printf("I/O error while interacting with ffmpeg", err)
+	// 		break
+	// 	}
+	// }
+	{
+		err := cmd.Wait()
+		if err != nil {
+			return fmt.Errorf("ffmpeg did not close properly? %w", err)
+		}
+	}
+
+	// chekc the file was written properly or not
+	outFileInfo, err := os.Stat(outpath)
+	if err != nil {
+		return fmt.Errorf("could not confirm output file written: %w", err)
+	}
+	movie.IsShrunk = true
+	movie.ShrunkSize = int(outFileInfo.Size())
+	return nil
+}
+
+func main() {
+	var src, dst string
+	flag.StringVar(&src, "src", ".", "The directory with the source video files")
+	flag.StringVar(&dst, "dst", "./smaller", "The directory where compressed videos are to be placed")
+
+	flag.Parse()
+
+	srcMovies := ListVideos(src)
+	dstMovies := ListVideos(dst)
+
+	movies := make([]VideoFileStatus, 0)
+	for _, srcEntry := range srcMovies {
+		var movie VideoFileStatus
+		movie.Name = srcEntry.Name
+		movie.Size = srcEntry.Size
+		movie.Src = src
+		movie.Dst = dst
+
+		// find a shrunk version of the movie
+		for _, dstEntry := range dstMovies {
+			if dstEntry.Name == srcEntry.Name {
+				movie.IsShrunk = true
+				movie.ShrunkSize = dstEntry.Size
+				break
+			}
+		}
+
+		movies = append(movies, movie)
+	}
+
+
+	for _, movie := range movies {
+		fmt.Printf("File: %s %s", movie.Name, BytesSize(movie.Size))
+		if movie.IsShrunk {
+			fmt.Printf(" -> %s", BytesSize(movie.ShrunkSize))
+		}
+		fmt.Println()
+	}
+
+	// pick the biggest file progressively and process it
+	for {
+		fmt.Println("looking for largest unprocessed file")
+		// TODO: allow the user to send a signal that this is the last file to process!
+		var largest *VideoFileStatus
+		for index := range movies {
+			movie := &movies[index]
+			if movie.IsShrunk {
+				continue
+			}
+			if largest == nil {
+				largest = movie
+			} else {
+				if movie.Size > largest.Size {
+					largest = movie
+				}
+			}
+		}
+		if largest == nil {
+			fmt.Println("It appears we have processed all the files!")
+			break
+		}
+		fmt.Println("Processing file:", largest.Name)
+		err := ShrinkMovie(largest)
+		if err != nil {
+			largest.HadError = true
+			fmt.Println("Warning: shrinking %s failed: %s", largest.Name, err)
+		}
+		break // TEMP only do it once for now!
+	}
+}
