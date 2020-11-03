0004b060195ddd7c0d9f733f03075df8cd57e4c4
Hasen Judy <hasen.judy@gmail.com>
2020-11-03 18:47:56 +0900

    add image support

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module "go.hasen.dev/media_shrinker"
+module go.hasen.dev/media_shrinker
 
 go 1.14
+
+require (
+	github.com/disintegration/imageorient v0.0.0-20180920195336-8147d86e83ec
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+)
--- pictures.go
+++ pictures.go
@@ -1,11 +1,68 @@
 package media_shrinker
 
+import "os"
+import "io"
+import "image"
+import "image/png"
+import "image/jpeg"
+import "github.com/disintegration/imageorient"
+import "github.com/nfnt/resize"
 import "fmt"
 
+func ResizeImage(img image.Image) image.Image {
+	size := img.Bounds().Size()
+	desiredWidth := 2048
+	if size.Y > size.X { // vertical image
+		desiredWidth = 1080
+	}
+	if size.X < desiredWidth {
+		desiredWidth = size.X // no scale up
+	}
+
+	return resize.Resize(uint(desiredWidth), 0, img, resize.Lanczos3)
+}
+
+func encodePng(out io.Writer, img image.Image) error {
+	encoder := png.Encoder {
+		CompressionLevel: png.BestCompression,
+	}
+	return encoder.Encode(out, img)
+}
+
+func encodeJpeg(out io.Writer, img image.Image) error {
+	options := jpeg.Options {
+		Quality: 90,
+	}
+	return jpeg.Encode(out, img, &options)
+}
+
+func ShrinkImage(request ProcessingRequest, encoder func(io.Writer, image.Image) error) error {
+	file, err := os.Open(request.InputPath)
+	if err != nil {
+		return fmt.Errorf("Could not open file %s: %w", request.InputPath, err)
+	}
+	defer file.Close()
+
+	img, _, err := imageorient.Decode(file)
+	if err != nil {
+		return fmt.Errorf("Could not decode file %s: %w", request.InputPath, err)
+	}
+
+	imgResized := ResizeImage(img)
+
+	out, err := os.Create(request.OutputPath)
+	if err != nil {
+		return fmt.Errorf("Could not create output file %s: %w", request.InputPath, err)
+	}
+	defer out.Close()
+
+	return encoder(out, imgResized)
+}
+
 func ShrinkPNG(request ProcessingRequest) error {
-	return fmt.Errorf("png processing not implemented")
+	return ShrinkImage(request, encodePng)
 }
 
 func ShrinkJPG(request ProcessingRequest) error {
-	return fmt.Errorf("jpg processing not implemented")
+	return ShrinkImage(request, encodeJpeg)
 }
